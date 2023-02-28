d9246d64ac03073a0d86af8d646920b453a0d131
manav083 <manav@locad.net>
2023-02-28 11:30:57 +0530

    input component added

--- client/src/components/Input/Input.jsx
+++ client/src/components/Input/Input.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import "Input.css";
+import React from "react";
+import "./Input.css";
 
-const Input = () => {
+const Input = (props) => {
   return (
-    <div>Input</div>
-  )
-}
+    <>
+      <div className="inputContainer">
+        <div className="label">
+          <label id={props.id} name={props.id}>
+            {props.label}
+          </label>
+        </div>
+        <input
+          type={props.type}
+          for={props.id}
+          value={props.value}
+          onChange={props.action}
+          required={props.require === "true" ? true : false}
+        />
+      </div>
+      <br />
+    </>
+  );
+};
 
-export default Input
+export default Input;
